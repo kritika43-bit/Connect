aa60521f67d8782290eb1c82dc6dd2ced3d28168
kritika43-bit <prasadkritika43@gmail.com>
2026-04-30 11:42:56 +0530

    Add request logging middleware

--- server/index.js
+++ server/index.js
@@ -19,6 +19,10 @@ const dashboardRoutes = require('./src/routes/dashboard');
 
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
+});
 
 // API Routes
 app.use('/api/auth', authRoutes);
